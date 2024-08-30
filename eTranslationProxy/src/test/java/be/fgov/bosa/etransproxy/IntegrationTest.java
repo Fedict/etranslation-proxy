36a51985596a34749ac4f1b56e789c363d244892
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2024-08-30 22:24:13 +0100

    GH-4: added initial test code

---
+++ eTranslationProxy/src/test/java/be/fgov/bosa/etransproxy/IntegrationTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2023, FPS BOSA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package be.fgov.bosa.etransproxy;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.junit.jupiter.MockServerExtension;
+import org.mockserver.junit.jupiter.MockServerSettings;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ *
+ * @author Bart Hanssens
+ */
+@Testcontainers
+@SpringBootTest
+@ExtendWith(MockServerExtension.class)
+@MockServerSettings(ports = {9999})
+public class IntegrationTest {
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> db = new PostgreSQLContainer<>("postgres:16-alpine")
+								.withInitScript("create_tables.sql");
+
+	private MockServerClient mock;
+
+    @BeforeEach
+    public void setup(MockServerClient mock) {
+        this.mock = mock;
+		mock.when(
+				request()
+					.withMethod("POST")
+					.withPath("/si/translate")
+				).respond(
+					response()
+						.withStatusCode(200)
+						.withBody("Mocked Response!!!!!!")
+						.withDelay(TimeUnit.SECONDS, 1)
+				);
+    }
+
+	@Test
+	void translateTest() {
+		
+	}
+
+
+}
